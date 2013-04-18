9456b98e659c5565581666cc8bad7105c254ae1f
Sam Williamson <sam.williamson@cqlcorp.com>
2013-04-18 13:11:03 -0400

    Guess checker

---
+++ src/Fives.Gameplay/GuessComparer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fives.Gameplay
+{
+	public class GuessComparer
+	{
+		ValidWord valid = new ValidWord();
+
+		public GuessResult CompareGuess(string guess, string word)
+		{
+			GuessResult result = new GuessResult();
+
+			if (valid.WordIsValid(guess))
+			{
+				if (guess.ToUpper() == word.ToUpper())
+				{
+					result.Matches = 5;
+					result.InPlace = 5;
+				}
+				else
+				{
+					// Use each letter only once
+					List<char> match = new List<char>();
+					List<int> inPlace = new List<int>();
+
+					// Check how many letters match and in place
+					for (int i = 0; i < 5; i++)
+					{ // Selected word is i
+						for (int j = 0; j < 5; j++)
+						{ // guess is j
+							if (guess[j] == word[i])
+							{
+								// Add to list of letters already tried 
+								// Increment matches
+								if (!match.Contains(Convert.ToChar(guess[i].ToString().ToUpper())))
+								{
+									match.Add(Convert.ToChar(guess[i].ToString().ToUpper()));
+									result.Matches++;
+								}
+
+								// Check if the letter is in the right place
+								if (!inPlace.Contains(j))
+								{
+									if (guess[j] == word[j])
+									{
+										result.InPlace++;
+										inPlace.Add(j);
+									}
+								}
+							}
+						}
+					}
+
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ test/Fives.Gameplay.Test/GuessComparerTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Fives.Gameplay.Test
+{
+	public class GuessComparerTests
+	{
+		[Fact]
+		public void GuessComparer_ExactMatch_ReturnsExactMatch()
+		{
+			GuessComparer comparer = new GuessComparer();
+			var result = comparer.CompareGuess("bread", "bread");
+
+			Assert.Equal(5, result.InPlace);
+			Assert.Equal(5, result.Matches);
+		}
+
+		[Fact]
+		public void GuessComparer_ExactMatch_ReturnsExactMatch_IgnoresCase()
+		{
+			GuessComparer comparer = new GuessComparer();
+			var result = comparer.CompareGuess("bread", "BREAD");
+
+			Assert.Equal(5, result.InPlace);
+			Assert.Equal(5, result.Matches);
+		}
+
+		[Fact]
+		public void GuessComparer_EmptyGuess_ReturnsNoMatch()
+		{
+			GuessComparer comparer = new GuessComparer();
+			var result = comparer.CompareGuess("", "BREAD");
+
+			Assert.Equal(0, result.InPlace);
+			Assert.Equal(0, result.Matches);
+		}
+
+		[Fact]
+		public void GuessComparer_InvalidGuess_ReturnsNoMatch_IgnoresCase()
+		{
+			GuessComparer comparer = new GuessComparer();
+			var result = comparer.CompareGuess("APPLE", "BREAD");
+
+			Assert.Equal(0, result.InPlace);
+			Assert.Equal(0, result.Matches);
+		}
+
+		[Fact]
+		public void GuessComparer_0Match_0InPlace()
+		{
+			GuessComparer comparer = new GuessComparer();
+			var result = comparer.CompareGuess("GHIJK", "ABCDE");
+
+			Assert.Equal(0, result.InPlace);
+			Assert.Equal(0, result.Matches);
+		}
+
+		[Fact]
+		public void GuessComparer_1Match_0InPlace()
+		{
+			GuessComparer comparer = new GuessComparer();
+			var result = comparer.CompareGuess("EFGHI", "ABCDE");
+
+			Assert.Equal(0, result.InPlace);
+			Assert.Equal(1, result.Matches);
+		}
+
+		[Fact]
+		public void GuessComparer_2Match_0InPlace()
+		{
+			GuessComparer comparer = new GuessComparer();
+			var result = comparer.CompareGuess("DEFGH", "ABCDE");
+
+			Assert.Equal(0, result.InPlace);
+			Assert.Equal(2, result.Matches);
+		}
+
+		[Fact]
+		public void GuessComparer_3Match_0InPlace()
+		{
+			GuessComparer comparer = new GuessComparer();
+			var result = comparer.CompareGuess("CDEFG", "ABCDE");
+
+			Assert.Equal(0, result.InPlace);
+			Assert.Equal(3, result.Matches);
+		}
+
+		[Fact]
+		public void GuessComparer_4Match_0InPlace()
+		{
+			GuessComparer comparer = new GuessComparer();
+			var result = comparer.CompareGuess("BCDEF", "ABCDE");
+
+			Assert.Equal(0, result.InPlace);
+			Assert.Equal(4, result.Matches);
+		}
+
+		[Fact]
+		public void GuessComparer_5Match_0InPlace()
+		{
+			GuessComparer comparer = new GuessComparer();
+			var result = comparer.CompareGuess("BCDEA", "ABCDE");
+
+			Assert.Equal(0, result.InPlace);
+			Assert.Equal(5, result.Matches);
+		}
+
+		[Fact]
+		public void GuessComparer_5Match_1InPlace()
+		{
+			GuessComparer comparer = new GuessComparer();
+			var result = comparer.CompareGuess("BCDAE", "ABCDE");
+
+			Assert.Equal(1, result.InPlace);
+			Assert.Equal(5, result.Matches);
+		}
+
+		[Fact]
+		public void GuessComparer_4Match_4InPlace()
+		{
+			GuessComparer comparer = new GuessComparer();
+			var result = comparer.CompareGuess("ABCDF", "ABCDE");
+
+			Assert.Equal(4, result.InPlace);
+			Assert.Equal(4, result.Matches);
+		}
+	}
+}
