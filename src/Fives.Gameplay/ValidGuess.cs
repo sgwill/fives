b3695d029d259c57ddaa4fe6ea033ed316daf029
Sam Williamson <sam.williamson@cqlcorp.com>
2013-04-18 12:29:44 -0400

    Added valid guess

---
+++ src/Fives.Gameplay/ValidGuess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Fives.Gameplay
+{
+	public class ValidGuess
+	{
+		public bool GuessIsValid(string word)
+		{
+			if (word.Length != 5)
+				return false;
+
+			if (word.Distinct().Count() != 5)
+				return false;
+
+			return true;
+		}
+	}
+}
---
+++ test/Fives.Gameplay.Test/ValidGuessTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xunit;
+
+namespace Fives.Gameplay.Test
+{
+	public class ValidGuessTests
+	{
+		[Fact]
+		public void FiveCharacters_IsValid()
+		{
+			ValidGuess guess = new ValidGuess();
+			Assert.True(guess.GuessIsValid("abcde"));
+		}
+
+		[Fact]
+		public void LessThanFiveCharacters_IsInValid()
+		{
+			ValidGuess guess = new ValidGuess();
+			Assert.False(guess.GuessIsValid("abcd"));
+		}
+
+		[Fact]
+		public void MoreThanFiveCharacters_IsInValid()
+		{
+			ValidGuess guess = new ValidGuess();
+			Assert.False(guess.GuessIsValid("abcdef"));
+		}
+
+		[Fact]
+		public void OnlyFiveDistinctCharacters_IsValid()
+		{
+			ValidGuess guess = new ValidGuess();
+			Assert.False(guess.GuessIsValid("aaaaa"));
+			Assert.False(guess.GuessIsValid("aaaab"));
+			Assert.False(guess.GuessIsValid("aaabc"));
+			Assert.False(guess.GuessIsValid("aabcd"));
+		}
+	}
+}
